710efa7818bb84d6aa7ddf4cf4ac7ff69039e897
LucasBara-dising <lucas.barauna098@gmail.com>
2021-07-27 18:30:35 -0300

    Telas Info hotel e favoritos prontas

---
+++ app/src/main/java/com/example/tickednow/Favortios.java
@@ -0,0 +1,14 @@
+package com.example.tickednow;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Favortios extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_favortios);
+    }
+}
